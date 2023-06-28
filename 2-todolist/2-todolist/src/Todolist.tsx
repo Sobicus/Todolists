12a826599d7609ab21008e3c9b16f1531eb41029
Sobicus <83043123+Sobicus@users.noreply.github.com>
2023-06-28 23:26:35 +0200

    update knowledge day 2 end

--- 2-todolist/2-todolist/src/Todolist.tsx
+++ 2-todolist/2-todolist/src/Todolist.tsx
@@ -19,9 +19,12 @@ export function Todolist(props: PropsType) {
             <button>+</button>
         </div>
         <ul>
-            <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-            <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-            <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+            {props.tasks.map(el => {
+                return <li>
+                    <input type="checkbox" checked={el.isDone}/>
+                    <span>{el.title}</span>
+                </li>
+            })}
         </ul>
         <div>
             <button>All</button>
