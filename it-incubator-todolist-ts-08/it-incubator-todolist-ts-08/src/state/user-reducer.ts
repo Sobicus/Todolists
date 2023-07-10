f9996544479b9676e321bcf2e1202dc356e9e566
Sobicus <83043123+Sobicus@users.noreply.github.com>
2023-07-10 19:01:50 +0200

    upd knowledge day12/lesson 8 mid

---
+++ it-incubator-todolist-ts-08/it-incubator-todolist-ts-08/src/state/user-reducer.ts
@@ -0,0 +1,26 @@
+type StateType = {
+    age: number
+    childrenCount: number
+    name: string
+}
+type ActionType = {
+    type: string
+    [key: string]: any
+}
+export const userReducer = (state: StateType, action: ActionType): StateType => {
+    switch (action.type) {
+        case 'INCREMENT-AGE': {
+            let newState = {...state, age: state.age + 1}
+            return newState
+        }
+        case 'INCREMENT-CHILDREN-COUNT': {
+
+            return {...state, childrenCount: state.childrenCount + 1}
+        }
+        case 'CHANGE-NAME': {
+            return {...state, name: action.newName}
+        }
+        default:
+            throw new Error('I don\'t understand this type')
+    }
+}
