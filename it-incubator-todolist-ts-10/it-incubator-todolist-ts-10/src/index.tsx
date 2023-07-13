f3400914e4859687866c659f63ad9d7890eb56a3
Sobicus <83043123+Sobicus@users.noreply.github.com>
2023-07-13 19:43:58 +0200

    upd knowledge day14/lesson10

--- it-incubator-todolist-ts-10/it-incubator-todolist-ts-10/src/index.tsx
+++ it-incubator-todolist-ts-10/it-incubator-todolist-ts-10/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './index.css';
-import App from './App';
 import { createRoot } from 'react-dom/client';
 import * as serviceWorker from './serviceWorker';
+import AppWithReducers from './AppWithReducers';
 
 const container  = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
-root.render(<App />);
+root.render(<AppWithReducers />);
 
 
 // If you want your app to work offline and load faster, you can change
